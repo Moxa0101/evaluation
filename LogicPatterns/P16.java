4a0e0bdc58aa61875cc80c62dfdc614bf3e41545
Moxa0101 <moxasanghavi@gmail.com>
2025-07-12 16:03:02 +0530

    Patterns

--- LogicPatterns/P16.java
+++ LogicPatterns/P16.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class P16{
     public static void main(String[] args) {
-        for(int i = 1 ;i<=5 ;i++){
+            Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i = 1 ;i<=n ;i++){
             for(int j=1; j<=i; j++){
                 System.out.print(i);
              }
