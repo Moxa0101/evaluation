a05ee1b0cd5cf2a49b91afcf291eafa72cd4ce51
Moxa0101 <moxasanghavi@gmail.com>
2025-07-12 16:02:29 +0530

    Patterns

--- LogicPatterns/P15.java
+++ LogicPatterns/P15.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class P15{
     public static void main(String[] args) {
       //i=row,j=blank space,k=column star
-        for(int i =6; i>=1; i-- ){
-            for (int j =1; j<=6-i; j++){
+            Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i =n; i>=1; i-- ){
+            for (int j =1; j<=n-i; j++){
                 System.out.print(" ");
             }
             for(int k = 1; k <=i ; k++){
@@ -11,8 +14,8 @@ public class P15{
                 }
      System.out.println();
         }
-            for(int i =2; i<=6; i++ ){
-                for (int j =1; j<=6-i; j++){
+            for(int i =2; i<=n; i++ ){
+                for (int j =1; j<=n-i; j++){
                 System.out.print(" ");
                  }
             for(int k = 1; k <=i ; k++){
