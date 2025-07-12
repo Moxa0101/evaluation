9afeb434f03bf97e1b8f82a3edd51d905614eb77
Moxa0101 <moxasanghavi@gmail.com>
2025-07-12 16:00:20 +0530

    Patterns

--- LogicPatterns/P14.java
+++ LogicPatterns/P14.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class P14{
     public static void main(String[] args) {
       //i=row,j=blank space,k=column star
-        for(int i =1; i<= 6; i++ ){
-            for (int j =1; j<=6-i; j++){
+         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i =1; i<= n; i++ ){
+            for (int j =1; j<=n-i; j++){
                 System.out.print(" ");
             }
           
@@ -12,8 +15,8 @@ public class P14{
                 }
                 System.out.println();
         }          
-            for(int i =5; i>=1; i-- ){
-                   for (int j=1; j<=6-i; j++){
+            for(int i =n-1; i>=1; i-- ){
+                   for (int j=1; j<=n-i; j++){
                 System.out.print(" ");
                    }
             
