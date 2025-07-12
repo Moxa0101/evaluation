a23462b929400dc4ea474f17d84ad8c15463ecb2
Moxa0101 <moxasanghavi@gmail.com>
2025-07-12 15:53:59 +0530

    P9.java

--- LogicPatterns/P9.java
+++ LogicPatterns/P9.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class P9{
     public static void main(String[] args) {
       //i=row,j=blamk space,k=column star
-        for(int i =1; i<= 6; i++ ){
-            for (int j =1; j<=6-i; j++){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n");
+        int n=sc.nextInt();
+        for(int i =1; i<= n; i++ ){
+            for (int j =1; j<=n-i; j++){
                 System.out.print(" ");
             }
             for(int k = 1; k <=i ; k++){
