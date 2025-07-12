a3f5947ef2fc5e3cf75b17ee65ccc34024618c5f
Moxa0101 <moxasanghavi@gmail.com>
2025-07-12 15:57:54 +0530

    P12.java

--- LogicPatterns/P12.java
+++ LogicPatterns/P12.java
@@ -1,13 +1,16 @@
 import java.util.*;
 public class P12{
     public static void main(String[] args) {
-      //i=row,j=blamk space,k=column star
-        for(int i =6; i>=1; i-- ){
-            for (int j =1; j<=6-i; j++){
+      //i=row,j=blamk space,k=column star+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i =n; i>=1; i-- ){
+            for (int j =1; j<=n-i; j++){
                 System.out.print(" ");
             }
             for(int k = 1; k <=i ; k++){
-                    System.out.print("* ");
+                    System.out.print("*");
                 }
      System.out.println();
         }
